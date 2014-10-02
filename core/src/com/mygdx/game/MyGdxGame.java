2bd698cbbe2df32ac11264ee846ca38ba05649a2
KenG98 <kengarber98@gmail.com>
2014-10-02 18:23:42 -0400

    almost final copy for club fair day

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -8,8 +8,13 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MyGdxGame extends ApplicationAdapter {
     private SpriteBatch batch;
@@ -22,7 +27,13 @@ public class MyGdxGame extends ApplicationAdapter {
     private Gem theGem;
     private Texture instructions;
     private int gameStage = 0; //0)instructions 1)gameplay 2)post death
-
+    private long startTime;
+    private TimeUtils timeutil;
+    private long totalTime = 0;
+    private BitmapFont finalStats;
+    private String finalString;
+    private String name;
+    private int gemsGotten;
 
     public MyGdxGame() {
     }
@@ -33,6 +44,7 @@ public class MyGdxGame extends ApplicationAdapter {
     }
 
     public void init() {
+        timeutil = new TimeUtils();
         batch = new SpriteBatch();
         mainchar = new MainCharacter(waveNumber);
         for (int i = 0; i < waveNumber; i++) {
@@ -40,6 +52,9 @@ public class MyGdxGame extends ApplicationAdapter {
         }
         theGem = new Gem();
         instructions = new Texture(Gdx.files.internal("data/instructions.png"));
+        finalStats = new BitmapFont();
+        finalStats.setColor(Color.BLACK);
+        finalStats.setScale(3);
     }
 
     @Override
@@ -57,7 +72,10 @@ public class MyGdxGame extends ApplicationAdapter {
                 batch.begin();
                 batch.draw(instructions,0,0);
                 batch.end();
-                if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)||Gdx.input.isKeyPressed(Input.Keys.LEFT)||Gdx.input.isKeyPressed(Input.Keys.UP)||Gdx.input.isKeyPressed(Input.Keys.DOWN)) gameStage = 1;
+                if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)||Gdx.input.isKeyPressed(Input.Keys.LEFT)||Gdx.input.isKeyPressed(Input.Keys.UP)||Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+                    gameStage = 1;
+                    startTime = timeutil.millis();
+                }
                 break;
             case 1:
                 rightKey = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
@@ -109,6 +127,85 @@ public class MyGdxGame extends ApplicationAdapter {
                 }
                 break;
             case 2:
+                //add code for final screen here.
+                batch.begin();
+                finalStats.setScale(3);
+                finalString = "YOU GOT: " + gemsGotten + " GEMS";
+                finalStats.draw(batch, finalString,20,400);
+                finalString = "IT TOOK " + totalTime + " SECONDS";
+                finalStats.draw(batch, finalString, 20, 300);
+                finalStats.setScale(1);
+                finalStats.draw(batch, "To submit your score, type your name",20, 200);
+                finalStats.draw(batch, "Press enter to continue",20, 20);
+                finalStats.draw(batch,name,20,100);
+                batch.end();
+
+                if(Gdx.input.isKeyJustPressed(Input.Keys.A)) name += "A";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.B)) name += "B";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.C)) name += "C";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.D)) name += "D";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.E)) name += "E";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.F)) name += "F";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.G)) name += "G";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.H)) name += "H";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.I)) name += "I";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.J)) name += "J";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.K)) name += "K";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.L)) name += "L";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.M)) name += "M";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.N)) name += "N";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.O)) name += "O";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.P)) name += "P";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.Q)) name += "Q";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.R)) name += "R";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.S)) name += "S";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.T)) name += "T";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.U)) name += "U";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.V)) name += "V";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.W)) name += "W";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.X)) name += "X";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.Y)) name += "Y";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.Z)) name += "Z";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) name += " ";
+                if(Gdx.input.isKeyJustPressed(Input.Keys.BACKSPACE)) name = name.substring(0,name.length() - 1);
+
+
+                if(Gdx.input.isKeyPressed(Input.Keys.ENTER)){
+                    gameStage = 0;
+                    //"gameStats.csv"
+
+                    File file = new File("gameStats.csv");
+                    // if file doesnt exists, then create it
+                    if (!file.exists()) {
+                        try {
+                            file.createNewFile();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    FileWriter fw = null;
+                    //BufferedWriter bw = null;
+                    try {
+                        //fw = new FileWriter(file.getAbsoluteFile());
+                        //bw = new BufferedWriter(fw);
+                        //bw.write(name + "," + (gemsGotten) + "," + totalTime);
+                        //bw.close();
+
+                        fw = new FileWriter(file,true);
+                        fw.write(name + "," + (gemsGotten) + "," + totalTime + "\n");
+                        fw.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+//                    try {
+//                        PrintStream filewriter = new PrintStream("gameStats.csv");
+//                        filewriter.println(name + "," + (gemsGotten) + "," + totalTime);
+//                    } catch (FileNotFoundException e) {
+//                        e.printStackTrace();
+//                    }
+                }
                 break;
         }
 
@@ -125,21 +222,32 @@ public class MyGdxGame extends ApplicationAdapter {
             blueEnemies.get(i).draw();
             if (blueEnemies.get(i).didCapture()) {
                 gameOver = true;
-                System.out.println("You got " + (waveNumber - 1) + " gems");
                 //Gdx.app.exit(); //instead of this, advance game stage to post-death stage
             }
         }
         if (gameOver){
+            long endTime = timeutil.millis();
+            totalTime = (endTime - startTime)/1000;
+            //move this all to the "final screen"
+            System.out.println("You got " + (waveNumber - 1) + " gems");
+            System.out.println("It took you " + totalTime + " seconds");
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
+            name = ""; //resets the name string
+
+
+
             gameOver = false;
-            gameStage = 0;
+            gameStage = 2;
+            gemsGotten = waveNumber - 1; //store the number of gems to save later
             waveNumber = 1;
             blueEnemies.clear();
             blueEnemies.add(new BlueArrowEnemy());
+            mainchar = new MainCharacter(1);
         }
     }
 }
